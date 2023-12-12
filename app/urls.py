a4f2949d96bc0610524750bec755e6dd0119648f
aneliia292 <144489728+aneliia292@users.noreply.github.com>
2023-12-12 19:26:52 +0300

    Add files via upload

---
+++ app/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path, re_path
+
+from .views import *
+
+
+class AppCategory:
+    pass
+
+
+class AppHome:
+    pass
+
+
+urlpatterns = [
+    path('', AppHome.as_view(), name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', AddPage.as_view(), name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    path('category/<slug:cat_slug>/', AppCategory.as_view(), name='category'),
+]
